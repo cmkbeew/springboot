5fe6227d98d85b8a369e0058f1753f6a13da19d1
chk <cmkbeew@naver.com>
2024-05-08 17:46:52 +0900

    init commit

---
+++ src/main/java/org/fullstack4/springboot/controller/SampleController.java
@@ -0,0 +1,26 @@
+package org.fullstack4.springboot.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Log4j2
+@Controller
+public class SampleController {
+
+    @GetMapping("/hello")
+    public void hello(Model model) {
+        model.addAttribute("msg", "HELLo~~~~~~~~~~~~~~~~~");
+    }
+
+    @GetMapping("/ex/ex1")
+    public void ex1(Model model) {
+        List<String> list = Arrays.asList("AAA", "BBB", "CCC");
+
+        model.addAttribute("list", list);
+    }
+}
---
+++ src/test/java/org/fullstack4/springboot/DataSourceTests.java
@@ -0,0 +1,31 @@
+package org.fullstack4.springboot;
+
+import lombok.Cleanup;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@Log4j2
+@SpringBootTest
+public class DataSourceTests {
+
+    @Autowired
+    private DataSource ds;
+
+    @Test
+    public void testConnection() throws SQLException {
+        @Cleanup
+        Connection conn = ds.getConnection();
+        log.info("===========================");
+        log.info("conn : " + conn);
+        log.info("===========================");
+
+        Assertions.assertNotNull(conn);
+    }
+}
